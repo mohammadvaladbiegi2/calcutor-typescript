0e57f2a3fdd75effb5ad5286693953fcbc22e091
mohammadvaladbiegi2 <126916758+mohammadvaladbiegi2@users.noreply.github.com>
2024-07-12 19:49:04 +0330

    complite develope project

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,14 @@
-"use strict";
+import { backspace, clearData, equalresult, handleKeydown } from "./utils.js";
+const equal = document.querySelector(".equal");
+const clearbtn = document.querySelector("#clear");
+const backspacebtn = document.querySelector("#backspace");
+window.addEventListener("keydown", handleKeydown);
+equal.addEventListener("click", () => {
+    equalresult();
+});
+clearbtn.addEventListener("click", () => {
+    clearData();
+});
+backspacebtn.addEventListener("click", () => {
+    backspace();
+});
--- dist/utils.js
+++ dist/utils.js
@@ -1,1 +1,105 @@
-"use strict";
+export let data = {
+    num1: 0,
+    num2: 0,
+    oparator: "",
+};
+export let numbersData = [];
+const mainNumber = document.querySelector(".main-number");
+const operators = document.querySelectorAll(".operator");
+const numbersbtn = document.querySelectorAll(".number");
+numbersbtn.forEach((num) => {
+    num.addEventListener("click", (e) => {
+        let target = e.target;
+        let value = target.value;
+        numbersData.push(value);
+        let concatnumbers = numbersData.join("");
+        if (!data.oparator.length) {
+            data.num1 = +concatnumbers;
+            updateMainNumber(concatnumbers);
+        }
+        else {
+            data.num2 = +concatnumbers;
+            updateMainNumber(concatnumbers);
+        }
+    });
+});
+operators.forEach((operator) => {
+    operator.addEventListener("click", (e) => {
+        let target = e.target;
+        let value = target.value;
+        data.oparator = value;
+        numbersData = [];
+        updateMainNumber("0");
+    });
+});
+export function calculatios(params) {
+    switch (params.oparator) {
+        case "+": {
+            return params.num1 + params.num2;
+        }
+        case "-": {
+            return params.num1 - params.num2;
+        }
+        case "x": {
+            return params.num1 * params.num2;
+        }
+        case "/": {
+            return params.num1 / params.num2;
+        }
+        default: {
+            let result = 0;
+            return result;
+        }
+    }
+}
+export const updateMainNumber = (value) => {
+    mainNumber.innerHTML = value;
+};
+export const backspace = () => {
+    numbersData.pop();
+    let concatnumbers = numbersData.join("");
+    if (!data.oparator.length) {
+        data.num1 = +concatnumbers;
+        updateMainNumber(concatnumbers);
+    }
+    else {
+        data.num2 = +concatnumbers;
+        updateMainNumber(concatnumbers);
+    }
+};
+export const equalresult = () => {
+    let result = calculatios(data);
+    let stringresulte = result.toString();
+    updateMainNumber(stringresulte);
+};
+export const clearData = () => {
+    data = {
+        num1: 0,
+        num2: 0,
+        oparator: "",
+    };
+    numbersData = [];
+    updateMainNumber("0");
+};
+export function handleKeydown(e) {
+    switch (e.keyCode) {
+        case 8: {
+            backspace();
+            break;
+        }
+        case 13: {
+            equalresult();
+            break;
+        }
+        case 27: {
+            clearData();
+            break;
+        }
+        default: {
+            return neverHandler(e);
+        }
+    }
+}
+export function neverHandler(e) {
+    throw new Error(`Unexpected event: ${e}`);
+}
